8d17c90bd171c9ce7a83eacbd159730c238d0a62
Cristian Lunardi <c.lunarditi@gmail.com>
2025-11-06 10:48:58 -0300

    feat: adiciona explicações sobre o código

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,14 +1,19 @@
+/* O Objetivo desse arquivo é carregar as variáveis de ambiente e validá-las usando o Zod */
+
 import 'dotenv/config'
 import { z } from 'zod'
 
+/* Definindo o esquema de validação para as variáveis de ambiente */
 const envSchema = z.object({
     NODE_ENV: z.enum(['development', 'test','production']).default('production'),
     DABATA_URL: z.string(),
     PORT: z.number().default(3333)
 })
 
+/* Validando as variáveis de ambiente carregadas do process.env */
 const _env = envSchema.safeParse(process.env)
 
+/* Se a validação falhar, exibe os erros e interrompe a aplicação */
 if (_env.success === false) {
     console.error('Invalid enviroment variables:', z.treeifyError(_env.error))
 
